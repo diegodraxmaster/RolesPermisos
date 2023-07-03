dfd07e7302e3974fa0b357ab91bac86add4b232c
Diego Armando Charpa Cerrato <diegodraxmaster@gmail.com>
2023-07-03 17:30:27 -0500

    refactor: :recycle: conexion.php

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -5,25 +5,18 @@ class Conexion{
 
 
     static public function conectar(){
+        $host = "localhost";
+        $dbname = "adminltmvc";
+        $username = "root";
+        $password = "";
 
-
-        $link = new PDO("mysql:host=localhost;dbname=adminltmvc","root","");
-
-        $link->exec("set names utf8mb4");
-
-        return $link;
-
+        try {
+            $conexion = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+            $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conexion;
+        } catch (PDOException $e) {
+            echo "Error al conectar a la base de datos: " . $e->getMessage();
+            exit;
+        }
     }
-
-    
-
-
-
-
 }
-
-
-
-
-
-?>
