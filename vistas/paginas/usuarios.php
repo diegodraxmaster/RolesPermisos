2ebee2c99df532fb332c3fffe3c05ef5154671ab
Diego Armando Charpa Cerrato <diegodraxmaster@gmail.com>
2023-07-03 22:46:01 -0500

    update: paginas.usuarios

--- vistas/paginas/usuarios.php
+++ vistas/paginas/usuarios.php
@@ -35,8 +35,7 @@
 
                         <div class="card-header">
 
-                            <button type="button" class="btn btn-success crear-usuarios" data-toggle="modal"
-                                data-target="#modal-crear-usuarios">
+                            <button type="button" class="btn btn-success crear-usuarios" data-toggle="modal" data-target="#modal-crear-usuarios">
                                 crear nuevo usuario
                             </button><br>
 
@@ -70,55 +69,51 @@
 
 
 
-                                    <?php 
-                                     foreach($usuarios as $key => $value){
+                                    <?php
+                                    foreach ($usuarios as $key => $value) {
 
 
-                                        $item="id_roles";
+                                        $item = "id_roles";
 
-                                        $valor =$value["rol"];
-
-
-                                        $roles = ctrRoles::ctrMostrarRoles($item ,$valor);
+                                        $valor = $value["rol"];
 
 
-                                                                      
+                                        $roles = ctrRoles::ctrMostrarRoles($item, $valor);
+
+
+
                                     ?>
 
-                                    <tr>
+                                        <tr>
 
-                                        <td><?php echo ($key+1)  ?></td>
-                                        <td><?php echo $value["nombre"]  ?></td>
-                                        <td><?php echo $value["usuario"]  ?></td>
-                                        <td><?php echo $roles["nom_rol"]  ?></td>
-                                        <td><img src="<?php echo $value["foto"]  ?>" width="40" height="40"></td>
+                                            <td><?php echo ($key + 1)  ?></td>
+                                            <td><?php echo $value["nombre"]  ?></td>
+                                            <td><?php echo $value["usuario"]  ?></td>
+                                            <td><?php echo $roles["nom_rol"]  ?></td>
+                                            <td><img src="<?php echo $value["foto"]  ?>" width="40" height="40"></td>
 
-                                        <td>
+                                            <td>
 
-                                            <div class='btn-group'>
+                                                <div class='btn-group'>
 
-                                                <button class="btn btn-warning btn-sm btnEditarUsuario"
-                                                    data-toggle="modal" idUsuario="<?php echo $value["id"]  ?>"
-                                                    data-target="#modal-editar-usuarios">
-                                                    <i class="fas fa-pencil-alt text-white"></i>
-                                                </button>
+                                                    <button class="btn btn-warning btn-sm btnEditarUsuario" data-toggle="modal" idUsuario="<?php echo $value["id"]  ?>" data-target="#modal-editar-usuarios">
+                                                        <i class="fas fa-pencil-alt text-white"></i>
+                                                    </button>
 
-                                                <button class="btn btn-danger btn-sm eliminarUsuario"
-                                                    idUsuarioE="<?php echo $value["id"]  ?>"
-                                                    rutaFoto="<?php echo $value["foto"]  ?>">
-                                                    <i class=" fas fa-trash-alt"></i>
-                                                </button>
+                                                    <button class="btn btn-danger btn-sm eliminarUsuario" idUsuarioE="<?php echo $value["id"]  ?>" rutaFoto="<?php echo $value["foto"]  ?>">
+                                                        <i class=" fas fa-trash-alt"></i>
+                                                    </button>
 
-                                            </div>
+                                                </div>
 
-                                        </td>
+                                            </td>
 
-                                    </tr>
+                                        </tr>
 
 
-                                    <?php 
+                                    <?php
 
-                                     }
+                                    }
 
                                     ?>
 
@@ -169,67 +164,70 @@ Modal Crear usuarios
                 <h4 class="alert alert-success alert-dismissible ">Agregar nuevo usuario</h4>
             </div>
             <form method="post" enctype="multipart/form-data">
-
-                <div class="form-group has-feedback" bis_skin_checked="1">
-                    <input type="text" class="form-control" name="nom_usuarios" placeholder="nombre">
-                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
-                </div>
-
-                <div class="form-group has-feedback" bis_skin_checked="1">
-                    <input type="text" class="form-control" name="nom_user" placeholder="usuario">
-                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-                </div>
-
-                <div class="form-group has-feedback" bis_skin_checked="1">
-                    <input type="password" class="form-control" name="pass_user" placeholder="password">
-                    <span class="glyphicon glyphicon-eye-close form-control-feedback"></span>
-                </div>
-
-                <div class="form-group has-feedback" bis_skin_checked="1">
-                    <div class="btn btn-default btn-file" bis_skin_checked="1">
-                        <i class="fas fa-paperclip"></i> Adjuntar Imagen de usuarios
-                        <input type="file" name="subirImgusuarios">
+                <div class="box-body">
+                    <div class="form-group has-feedback" bis_skin_checked="1">
+                        <label for="Nick_Name">Nick Name</label>
+                        <input type="text" class="form-control" name="nom_usuarios" placeholder="nombre">
+                        <span class="glyphicon glyphicon-user form-control-feedback"></span>
                     </div>
-                    <img class="previsualizarImgusuarios img-fluid py-2" width='200' height='200'>
-                    <p class="help-block small"> Dimensiones: 480px * 382px | Peso Max. 2MB | Formato: JPG o PNG</p>
+
+                    <div class="form-group has-feedback" bis_skin_checked="1">
+                        <label for="Nombre_usuario">Nombre usuario</label>
+                        <input type="text" class="form-control" name="nom_user" placeholder="usuario">
+                        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                    </div>
+
+                    <div class="form-group has-feedback" bis_skin_checked="1">
+                        <label for="Password">Password</label>
+                        <input type="password" class="form-control" name="pass_user" placeholder="password">
+                        <span class="glyphicon glyphicon-eye-close form-control-feedback"></span>
+                    </div>
+
+                    <div class="form-group has-feedback" bis_skin_checked="1">
+                        <div class="btn btn-default btn-file" bis_skin_checked="1">
+                            <i class="fas fa-paperclip"></i> Adjuntar Imagen de usuarios
+                            <input type="file" name="subirImgusuarios">
+                        </div>
+                        <img class="previsualizarImgusuarios img-fluid py-2" width='200' height='200'>
+                        <p class="help-block small"> Dimensiones: 480px * 382px | Peso Max. 2MB | Formato: JPG o PNG</p>
+                    </div>
+
+
+
+
+
+                    <div class="form-group has-feedback">
+
+
+                        <label>rol</label>
+                        <select class="form-control" name="rol_user" required>
+
+                            <?php
+                            $roles = ctrRoles::ctrMostrarRoles2();
+
+                            foreach ($roles as $rol) {
+
+                            ?>
+                                <option value="<?php echo $rol["id_roles"] ?>"><?php echo $rol["nom_rol"] ?></option>
+                            <?php
+                            }
+                            ?>
+
+                        </select>
+
+                    </div>
                 </div>
-
-
-
-
-
-                <div class="form-group has-feedback">
-
-
-                    <label>rol</label>
-                    <select class="form-control" name="rol_user" required>
-
-                        <?php
-                        $roles = ctrRoles::ctrMostrarRoles2();
-                        
-                        foreach($roles as $rol){
-                            
-?>
-                        <option value="<?php echo $rol["id_roles"] ?>"><?php echo $rol["nom_rol"] ?></option>
-                        <?php
-                       }
-?>
-
-                    </select>
-
-                </div>
-
                 <div class="modal-footer">
                     <button type="button" class="btn btn-danger pull-left" data-dismiss="modal">cerrar</button>
                     <button type="submit" class="btn btn-primary">guardar</button>
                 </div>
 
-                <?php 
+                <?php
 
                 $guardarusuarios = new ctrUsuarios();
                 $guardarusuarios->ctrGuardarusuarios();
-                
-                
+
+
                 ?>
 
 
@@ -259,8 +257,7 @@ Modal EDITAR usuarios
 
                 <div class="form-group has-feedback" bis_skin_checked="1">
                     <input type="hidden" id="idPerfilE" name="idPerfilE">
-                    <input type="text" class="form-control" id="nom_usuariosE" name="nom_usuariosE"
-                        placeholder="nombre">
+                    <input type="text" class="form-control" id="nom_usuariosE" name="nom_usuariosE" placeholder="nombre">
                     <span class="glyphicon glyphicon-user form-control-feedback"></span>
                 </div>
 
@@ -271,8 +268,7 @@ Modal EDITAR usuarios
 
                 <div class="form-group has-feedback" bis_skin_checked="1">
                     <input type="hidden" id="pass_userActualE" name="pass_userActualE">
-                    <input type="password" class="form-control" id="pass_userE" name="pass_userE"
-                        placeholder="password">
+                    <input type="password" class="form-control" id="pass_userE" name="pass_userE" placeholder="password">
                     <span class="glyphicon glyphicon-eye-close form-control-feedback"></span>
                 </div>
 
@@ -297,14 +293,14 @@ Modal EDITAR usuarios
 
                         <?php
                         $roles = ctrRoles::ctrMostrarRoles2();
-                        
-                        foreach($roles as $rol){
-                            
-?>
-                        <option value="<?php echo $rol["id_roles"] ?>"><?php echo $rol["nom_rol"] ?></option>
+
+                        foreach ($roles as $rol) {
+
+                        ?>
+                            <option value="<?php echo $rol["id_roles"] ?>"><?php echo $rol["nom_rol"] ?></option>
                         <?php
-                       }
-?>
+                        }
+                        ?>
 
                     </select>
 
@@ -315,12 +311,12 @@ Modal EDITAR usuarios
                     <button type="submit" class="btn btn-primary">editar</button>
                 </div>
 
-                <?php 
+                <?php
 
                 $editarusuarios = new ctrUsuarios();
                 $editarusuarios->ctrEditarusuarios();
-                
-                
+
+
                 ?>
 
 
